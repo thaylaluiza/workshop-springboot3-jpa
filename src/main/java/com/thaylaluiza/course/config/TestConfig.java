d312246518d8648166ec26091246cd281edc4bcc
Thayla Luiza <thaylaluiza000@gmail.com>
2025-08-06 02:42:59 -0300

    Category

--- src/main/java/com/thaylaluiza/course/config/TestConfig.java
+++ src/main/java/com/thaylaluiza/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.thaylaluiza.course.config;
 
 
+import com.thaylaluiza.course.entities.Category;
 import com.thaylaluiza.course.entities.Order;
 import com.thaylaluiza.course.entities.enums.OrderStatus;
+import com.thaylaluiza.course.repositories.CategoryRepository;
 import com.thaylaluiza.course.repositories.OrderRepository;
 import com.thaylaluiza.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +22,24 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
 
 
-
-
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/thaylaluiza/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.thaylaluiza.course.services;
+
+import com.thaylaluiza.course.entities.Category;
+import com.thaylaluiza.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
